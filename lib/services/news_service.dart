4d9a9ef171f8fcd56d7bcd5a01d6cdb561cc503b
RANA ASHRAF <ra3748797@gmail.com>
2024-10-20 15:17:20 +0300

    make service class and fetch articles data

---
+++ lib/services/news_service.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+
+
+class NewsService {
+  final dio = Dio();
+
+  genNews() async {
+    Response response = await dio.get(
+        'https://newsapi.org/v2/top-headlines?apiKey=4f808119ea5249b39ff6b21ae0caa11e&category=general');
+
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articlesData = jsonData['articles'];
+    print(articlesData);
+  }
+}
