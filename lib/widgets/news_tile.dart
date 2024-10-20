746acc4e6b60f2c655e9d2b2aa1935ddeff81229
RANA ASHRAF <ra3748797@gmail.com>
2024-10-20 18:06:28 +0300

    remove calling method general news from main && use statful widget in stead of statless widget beacause the ui change and also statefull make me call  general news method && use init state to call general method once when initializing the statefull widget && use set state to rebuild the ui when the data is fetched

--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
 
 class NewsTile extends StatelessWidget {
-  const NewsTile({
+  NewsTile({
+    required this.articleModel,
     super.key,
   });
-
+  ArticleModel articleModel;
   @override
   Widget build(BuildContext context) {
     return Column(
-    
       children: [
         ClipRRect(
           borderRadius: BorderRadius.circular(6),
           child: Image.network(
-            'https://media.istockphoto.com/id/1916729901/photo/meeting-success-two-business-persons-shaking-hands-standing-outside.jpg?s=1024x1024&w=is&k=20&c=lDdAFbi0F3tfUu5tQMgTRbgQ-wKPPoltpzmKc9rc2D4=',
+            articleModel.image??'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/rockcms/2024-10/241018-brent-taylor-migrant-construction-mn-1430-750480.jpg',
             height: 200,
             width: double.infinity,
             fit: BoxFit.fill,
           ),
         ),
-        const SizedBox(height: 8,),
-        const Text(
-          'the title should be herekn this place so drop it here please the tithe should be herekn this place so drop it here please',
-          style: TextStyle(
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          articleModel.title,
+           style:const TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
             color: Colors.black,
@@ -30,10 +33,12 @@ class NewsTile extends StatelessWidget {
           overflow: TextOverflow.ellipsis,
           maxLines: 2,
         ),
-        const SizedBox(height: 5,),
-        const Text(
-          'the description should be herekn this place so drop it here please the tithe should be herekn this place so drop it here please',
-          style: TextStyle(
+        const SizedBox(
+          height: 5,
+        ),
+         Text(
+          articleModel.subTitle?? '',
+            style:const TextStyle(
               fontSize: 16, fontWeight: FontWeight.w500, color: Colors.grey),
           overflow: TextOverflow.ellipsis,
           maxLines: 2,
--- lib/widgets/news_tile_list_view.dart
+++ lib/widgets/news_tile_list_view.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/models/article_model.dart';
+import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_tile.dart';
 
-class NewsTileListView extends StatelessWidget {
+class NewsTileListView extends StatefulWidget {
   const NewsTileListView({
     super.key,
   });
 
   @override
+  State<NewsTileListView> createState() => _NewsTileListViewState();
+}
+
+class _NewsTileListViewState extends State<NewsTileListView> {
+  List<ArticleModel> articles = [];
+  @override
+  void initState(){
+    super.initState();
+    getGeneralNews();
+  }
+
+  Future<void> getGeneralNews() async {
+      articles= await NewsService().generalNews();
+      setState(() {
+        
+      });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return   SliverList(
-        delegate: SliverChildBuilderDelegate(childCount: 10, (context, route) {
-      return const NewsTile();
-    }));
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: articles.length,
+        (context, index) {
+          return  NewsTile(articleModel: articles[index],);
+        },
+      ),
+    );
   }
 }
