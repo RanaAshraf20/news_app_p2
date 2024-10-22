a74c63776c6186154d6a0ee4e238d837a0b0aaae
RANA ASHRAF <ra3748797@gmail.com>
2024-10-22 03:47:28 +0300

    use future builder widget it used if i get data in the future and display it , future tigger the method that fetch the data and listen if the data come ,builder take an anomynous funcontext and snapshot. when the data come it is stored in the snapshot

--- lib/widgets/news_list_tile_builder.dart
+++ lib/widgets/news_list_tile_builder.dart
@@ -1,43 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_tile_list_view.dart';
 
-class NewsTileListViewBuilder extends StatefulWidget {
-  const NewsTileListViewBuilder({
-    super.key,
-  });
 
-  @override
-  State<NewsTileListViewBuilder> createState() =>
-      _NewsTileListViewBuilderState();
-}
+class  NewsTileListViewBuilder extends StatelessWidget {
+  const NewsTileListViewBuilder({super.key});
 
-class _NewsTileListViewBuilderState extends State<NewsTileListViewBuilder> {
-  List<ArticleModel> articles = [];
-  bool isLoading = true;
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService().generalNews();
-    isLoading = false;
-    setState(() {});
-  }
-
+ 
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverToBoxAdapter(
-            child: Center(child: CircularProgressIndicator()),
-          )
-        : articles != []
-            ? NewsTileListView(articles: articles)
-            : const SliverToBoxAdapter(
-                child: Center(child: Text('oops there is a problem, try later!')),
-              );
+    return FutureBuilder(
+      future: NewsService().generalNews(),
+      builder: (context, snapshot) {
+        return NewsTileListView(articles: snapshot.data ?? []);
+      },
+    );
+    // return isLoading
+    //     ? const SliverToBoxAdapter(
+    //         child: Center(child: CircularProgressIndicator()),
+    //       )
+    //     : articles.isNotEmpty
+    //         ? NewsTileListView(articles: articles)
+    //         : const SliverToBoxAdapter(
+    //             child:
+    //                 Center(child: Text('oops there is a problem, try later!')),
+    //           );
   }
 }
