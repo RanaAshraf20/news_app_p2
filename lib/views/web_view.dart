111685c36637158c9229de81939ad07724d58953
RANA ASHRAF <ra3748797@gmail.com>
2024-10-22 19:14:16 +0300

    make web view widget to display an article in the app

---
+++ lib/views/web_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebView extends StatefulWidget {
+  const WebView({super.key, required this.url});
+  final String url; 
+  @override
+  State<WebView> createState() => _WebViewState();
+}
+
+class _WebViewState extends State<WebView> {
+  late WebViewController controller;
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse(widget.url));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebViewWidget(controller: controller),
+    );
+  }
+}
