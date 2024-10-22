a9342be75ea758c2c8fd0b269903b5664686b1a0
RANA ASHRAF <ra3748797@gmail.com>
2024-10-23 01:19:21 +0300

    make the row take the size of the text not additional space so that it can be it the center of the app bar

--- lib/views/category_view.dart
+++ lib/views/category_view.dart
@@ -4,18 +4,43 @@ import 'package:news_app/models/category_model.dart';
 import 'package:news_app/widgets/news_list_tile_builder.dart';
 
 class CategoryView extends StatelessWidget {
-   const CategoryView({super.key, required this.categoryName});
-final String categoryName;
+  const CategoryView({super.key, required this.categoryName});
+  final String categoryName;
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-body: Center(
-  child: CustomScrollView(
-    slivers: [
-     NewsTileListViewBuilder(category:categoryName,)
-    ],
-  ),
-)
-    );
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          title: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'News',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Clouds',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: CustomScrollView(
+              slivers: [
+                NewsTileListViewBuilder(
+                  category: categoryName,
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
