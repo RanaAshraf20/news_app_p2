507b97a938382ecaa81eae8c6e7b57b4076206d4
RANA ASHRAF <ra3748797@gmail.com>
2024-10-24 12:27:21 +0300

    use custom app bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/components/customAppBar.dart';
 import 'package:news_app/models/category_model.dart';
-import 'package:news_app/views/search_view.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
 import 'package:news_app/widgets/news_list_tile_builder.dart';
 
@@ -19,39 +19,8 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          elevation: 0,
-          leading: IconButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return SearchView();
-              }));
-            },
-            icon: Icon(
-              Icons.search,
-              size: 30,
-            ),
-          ),
-          title: const Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                'News',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              Text(
-                'Clouds',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
-                ),
-              ),
-            ],
-          ),
-        ),
-        body: Padding(
+        appBar: const CustomAppBar(showSearchIcon: true),
+         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16),
           child: CustomScrollView(
             physics: const BouncingScrollPhysics(),
@@ -69,17 +38,6 @@ class HomeView extends StatelessWidget {
               const NewsTileListViewBuilder(category: 'General')
             ],
           ),
-          // child: Column(
-          //   children: [
-          //     CategorisListView(
-          //       categoryList: category,
-          //     ),
-          //    const SizedBox(
-          //       height: 32,
-          //     ),
-          //     const Expanded(child: NewsTileListView()),
-          //   ],
-          // ),
         ));
   }
 }
