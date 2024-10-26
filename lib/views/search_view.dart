102220f4d383f660b6a68ec1b27597334bec5dfc
RANA ASHRAF <ra3748797@gmail.com>
2024-10-26 23:28:04 +0300

    use textInputField custom component

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/components/customAppBar.dart';
+import 'package:news_app/shared_components/text_input_field.dart';
 import 'package:news_app/widgets/news_list_tile_builder.dart';
 
 class SearchView extends StatefulWidget {
@@ -17,37 +18,30 @@ class _SearchViewState extends State<SearchView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:CustomAppBar() ,
-       body: Padding(
+      appBar: CustomAppBar(),
+      body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: CustomScrollView(
           slivers: [
             SliverToBoxAdapter(
-              child: TextField(
-                onChanged: (value) {
-                  textQuery = value;
-                  setState(() {
-                    
-                  });
-                },
-                controller: controller,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    hintText: 'Search...',
-                    hintStyle: TextStyle(color: Colors.grey),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.orange))),
-              ),
-            ),
+                child: TextInputField(
+              controller: controller,
+              onSubmitted: (value) {
+                textQuery = value;
+                setState(() {});
+              },
+              hintText: 'Search...',
+              focusedBorderColor: Colors.orange,
+            )),
             SliverToBoxAdapter(
               child: SizedBox(
                 height: 30,
               ),
-            ), 
-            NewsTileListViewBuilder(category: 'general',word: textQuery,)
+            ),
+            NewsTileListViewBuilder(
+              category: 'general',
+              word: textQuery,
+            )
           ],
         ),
       ),
