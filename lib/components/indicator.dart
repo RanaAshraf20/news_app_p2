021b44009c49b96766f2ae3451d575498f317fed
RANA ASHRAF <ra3748797@gmail.com>
2024-10-26 23:36:54 +0300

    make color as variable

--- lib/components/indicator.dart
+++ lib/components/indicator.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 class Indicator extends StatelessWidget {
   const Indicator({
     super.key,
+    this.color = Colors.grey,
   });
-
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircularProgressIndicator(color: Colors.orange);
+    return CircularProgressIndicator(color: color);
   }
 }
