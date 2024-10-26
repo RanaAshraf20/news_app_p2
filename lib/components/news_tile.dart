adbe181fe341858e16811504c93b33238402ce51
RANA ASHRAF <ra3748797@gmail.com>
2024-10-26 23:23:25 +0300

    rename text class name

--- lib/components/news_tile.dart
+++ lib/components/news_tile.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/components/articleText.dart';
 import 'package:news_app/components/cachedImage.dart';
 import 'package:news_app/models/article_model.dart';
+import 'package:news_app/shared_components/customText.dart';
 import 'package:news_app/views/web_view.dart';
-
 
 class NewsTile extends StatelessWidget {
   const NewsTile({
@@ -16,28 +15,36 @@ class NewsTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-          return WebView(
-            url: articleModel.url,
-          );
-        }));
-      },
-      child: Column(
-        children: [
+        onTap: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return WebView(
+              url: articleModel.url,
+            );
+          }));
+        },
+        child: Column(children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(6),
             child: CachedImage(
-                image: articleModel.image ??
-                    "https://www.svgrepo.com/show/340721/no-image.svg"),
+              image: articleModel.image,
+              alternativeImage:
+                  "https://www.svgrepo.com/show/340721/no-image.svg",
+            ),
           ),
           const SizedBox(height: 4),
-          ArticleText(isTitle: true, text: articleModel.title),
+
+          CustomText(
+            text: articleModel.title,
+            weight: FontWeight.bold,
+          ), // CustomText
           const SizedBox(height: 2),
-          ArticleText(
-              text: articleModel.subTitle ?? 'No Description Available'),
-        ],
-      ),
-    );
+          CustomText(
+            text: articleModel.subTitle ?? 'No Description Available',
+            size: 16,
+            weight: FontWeight.w500,
+            color: Colors.grey,
+// CustomText
+          )
+        ]));
   }
 }
