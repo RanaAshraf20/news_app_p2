fd7632adf734ad602459328e21e9205d0779cb49
RANA ASHRAF <ra3748797@gmail.com>
2024-10-27 00:02:35 +0300

    remove the unusable file path

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/shared_components/articleText.dart';
 import 'package:news_app/models/category_model.dart';
+import 'package:news_app/shared_components/customText.dart';
 import 'package:news_app/views/category_view.dart';
 
 class CategoryCard extends StatelessWidget {
