c6429689d20c6aa936a62ecd2ba84afc936db087
RANA ASHRAF <ra3748797@gmail.com>
2024-10-26 22:59:21 +0300

    rename the text class name

--- lib/components/category_card.dart
+++ lib/components/category_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/shared_components/articleText.dart';
 import 'package:news_app/models/category_model.dart';
 import 'package:news_app/views/category_view.dart';
 
@@ -26,15 +27,12 @@ class CategoryCard extends StatelessWidget {
           ),
         ),
         child: Center(
-          child: Text(
-            categoryItem.categoryName,
-            style: const TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            ),
-          ),
-        ),
+            child: CustomText(
+          text: categoryItem.categoryName,
+          size: 20,
+          weight: FontWeight.bold,
+          color: Colors.white,
+        )),
       ),
     );
   }
